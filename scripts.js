20d0877e0ce3a99e344d36a0b588bd8d6f930ee3
terrifricker <terrifricker@yahoo.com>
2021-07-04 05:49:53 -0700

    js: moved form event listener to two event listeners, one on the message button, the other on the username buttons

--- scripts.js
+++ scripts.js
@@ -1,13 +1,22 @@
-buttons = document.querySelector('.buttons');
-form = document.querySelector('form');
+const buttons = document.querySelector('.buttons');
+const messageButton = document.querySelector('#send-message');
+const usernameButton = document.querySelector('#update-username');
 
 
 // default values
 let currentChatroom='web-development';
 let currentUsername='anonymous';
 
+
+buttons.addEventListener('click', e => {
+    console.log(e.target.id);
+    let currentChatroom = e.target.id;
+    console.log(currentChatroom);
 });
 
-form.addEventListener('click', (e) => {
-
-});
+messageButton.addEventListener('click', e => {
+    console.log(e.target.id);
+})
+usernameButton.addEventListener('click', e =>{
+    console.log(e.target.id);
+})
