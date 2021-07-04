99406f724c0eaa9f94302cbc3fd4628281f1d84e
terrifricker <terrifricker@yahoo.com>
2021-07-03 20:29:40 -0700

    js: add eventListeners for the chat room buttons and the form

---
+++ scripts.js
@@ -0,0 +1,11 @@
+buttons = document.querySelector('.buttons');
+form = document.querySelector('form');
+
+
+buttons.addEventListener('click', (e) => {
+
+});
+
+form.addEventListener('click', (e) => {
+
+});
