182ec219859d6f3fb47a75c07074df05798239cc
terrifricker <terrifricker@yahoo.com>
2021-07-04 06:13:03 -0700

    js: a click on a chatroom button changes currentChatroom variable and the background color of the message container

--- scripts.js
+++ scripts.js
@@ -6,12 +6,32 @@ const usernameButton = document.querySelector('#update-username');
 // default values
 let currentChatroom='web-development';
 let currentUsername='anonymous';
+let webDevColor = 'teal';
+let htmlColor = 'purple';
+let cssColor = 'red';
+let javascriptColor = 'green';
 
 
 buttons.addEventListener('click', e => {
-    console.log(e.target.id);
     let currentChatroom = e.target.id;
-    console.log(currentChatroom);
+    let color = document.querySelector('#message-window');
+    switch (currentChatroom) {
+        case 'web-development':
+            color.className = webDevColor;
+            break;
+        case 'html':
+            color.className = htmlColor;
+            break;
+        case 'css':
+            color.className = cssColor;
+            break;
+        case 'javascript':
+            color.className = javascriptColor;
+            break;
+        default:
+            color.className = webDevColor;
+    }
+
 });
 
 messageButton.addEventListener('click', e => {
